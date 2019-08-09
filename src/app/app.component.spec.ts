d6168d3b8645950a7181700d4c6f1c33037ef500
Fiona Carroll <fiona@Fionas-MacBook-Air.local>
2019-08-09 22:52:23 +0100

    work update

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,7 +26,7 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('HealthTrak');
   });
 
-  it('should render title in a title tag', () => {
+  it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
